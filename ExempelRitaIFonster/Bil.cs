7da6e9c8f0fe3ecc38e1ba57d88701751e2270dc
Viktor Jonsson <viktorjonsson93@gmail.com>
2012-10-04 00:02:52 +0200

    alla svängar funkar, grafikbugg(bilen)

--- ExempelRitaIFonster/Bil.cs
+++ ExempelRitaIFonster/Bil.cs
@@ -12,6 +12,8 @@ namespace ExempelRitaIFonster
 
     class Bil
     {
+        Random rand = new Random();
+        int a;
         int centerX;
         int centerY;
         int tileSize;
@@ -29,35 +31,97 @@ namespace ExempelRitaIFonster
 
         public void move()
         {
-            Random rand = new Random();
-            int a;
+            a = rand.Next(0, 2);
+            //måste vänster och vänstersväng i trevägskorsning
             if (map[centerY/20, centerX/20] == 2)
             {
-                if ((map[(centerY-20)/20, centerX/20] == 0 && map[centerY/20, (centerX-20)/20] == 0) ||
-                    (map[(centerY+20)/20, centerX/20] == 0 && map[centerY / 20, (centerX-20)/20] == 0) ||
-                    (map[centerY/20, (centerX+20)/20] == 0 && map[(centerY+20)/20, centerX/20]== 0) ||
-                    (map[(centerY-20)/20, centerX/20] == 0 && map[centerY/20, (centerX+20)/20] == 0))
+                //måste vänster
+                if ((dir == Riktning.upp && map[(centerY-20)/20, centerX/20] == 0) ||
+                    (dir == Riktning.ner && map[(centerY+20)/20, centerX/20] == 0) ||
+                    (dir == Riktning.höger && map[centerY/20, (centerX+20)/20] == 0) ||
+                    (dir == Riktning.vänster && map[centerY/20, (centerX-20)/20] == 0))
                 {
-                    bytRiktning(Riktning.vänster);
+                    bytRiktning(Riktning.vänster);    
                 }
-                if ((map[(centerY - 40) / 20, centerX / 20] == 0 && map[centerY / 20, (centerX + 40) / 20] == 0) ||
-                    (map[(centerY + 40) / 20, centerX / 20] == 0 && map[centerY / 20, (centerX + 40) / 20] == 0) ||
-                    (map[(centerY + 40) / 20, centerX / 20] == 0 && map[centerY / 20, (centerX - 40) / 20] == 0) || 
-                    (map[(centerY-40)/20, centerX/20] == 0 && map[centerY/20, (centerX-40)/20] == 0))
+                //kan svänga vänster
+                else if ((dir == Riktning.vänster && map[centerY/20, (centerX-20)/20] == 1) ||
+                    (dir == Riktning.upp && map[(centerY-20)/20, centerX/20] == 1) ||
+                    (dir == Riktning.ner && map[(centerY+20)/20, centerX/20] == 1) ||
+                    (dir == Riktning.höger && map[centerY/20, (centerX+20)/20] == 1))
+                {
+                    if (a == 1)
+                    {
+                        bytRiktning(Riktning.vänster);
+                    }
+                }
+                
+            }
+            //måste höger och högersväng i trevägskorsning
+            if (map[centerY/20, centerX/20]==3)
+            {
+                //måste höger
+                if ((dir == Riktning.upp && map[centerY/20, (centerX-20)/20] == 1) ||
+                    (dir == Riktning.ner && map[centerY/20, (centerX+20)/20] == 1) ||
+                    (dir == Riktning.höger && map[(centerY-20)/20, centerX/20] == 1) ||
+                    (dir == Riktning.vänster && map[(centerY+20)/20, centerX/20] == 1))
                 {
                     bytRiktning(Riktning.höger);
                 }
-                if ((map[(centerY+20)/20, centerX/20]==2 && map[centerY, (centerX+20)/20] == 2))
+                //kan svänga höger
+                else if ((dir == Riktning.höger && map[centerY/20, (centerX+20)/20] == 3) ||
+                    (dir == Riktning.vänster && map[centerY/20, (centerX-20)/20] == 3) || (dir == Riktning.vänster && map[centerY/20, (centerX-20)/20] == 2) ||
+                    (dir == Riktning.upp && map[(centerY-20)/20, centerX/20] == 3) || (dir == Riktning.upp && map[(centerY-20)/20, centerX/20] == 2) ||
+                    (dir == Riktning.ner && map[(centerY+20) / 20, centerX / 20] == 3))
                 {
-                    a = rand.Next(0, 1);
-                    if (a==1)
+                    if (a == 1)
                     {
                         bytRiktning(Riktning.höger);
                     }
                 }
             }
+            //4-vägskorsning
+            if (map[centerY/20, centerX/20] == 4)
+            {
+                //högersväng
+                if ((dir == Riktning.upp && map[(centerY+20)/20, centerX/20] == 4)||
+                    (dir == Riktning.ner && map[(centerY-20)/20, centerX/20] == 4) ||
+                    (dir == Riktning.höger && map[centerY/20, (centerX+20)/20] == 4)||
+                    (dir == Riktning.vänster && map[centerY/20, (centerX-20)/20] == 4))
+                {
+                    if (a==1)
+                    {
+                        bytRiktning(Riktning.höger);
+                    }
+                }
+                //vänstersväng
+                if ((dir == Riktning.upp && map[(centerY+20)/20, centerX/20] == 1) ||
+                    (dir == Riktning.vänster && map[centerY/20, (centerX-20)/20] == 1) ||
+                    (dir == Riktning.ner && map[(centerY+20)/20, centerX/20] == 1) ||
+                    (dir == Riktning.höger && map[centerY/20, (centerX+20)/20] == 1))
+                {
+                    if (a==1)
+                    {
+                        bytRiktning(Riktning.vänster);
+                    }
+                }
+            }
+            //3-vägskorsning
+            /*if (map[centerY/20, centerX/20] == 5)
+            {
+                //högersväng
+                if ((dir == Riktning.upp && map[(centerY+20)/20, centerX/20] == 5) ||
+                    (dir == Riktning.höger && map[centerY/20, (centerX+20)/20] == 5) ||
+                    (dir == Riktning.vänster && map[centerY/20, (centerX-20)/20] == 5) ||
+                    (dir == Riktning.ner && map[(centerY-20)/20, centerX/20] == 5))
+                {
+                    if (a==1)
+                    {
+                        bytRiktning(Riktning.höger);
+                    }
+                }
+            }*/
             
-            //öka på lämplig kordinat - baserat på bilens nuvarande riktning
+            //flytta bilen 20px i riktning
             if (dir == Riktning.höger)
                 centerX += tileSize;
             if (dir == Riktning.vänster)
